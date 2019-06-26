7c376c6f39ad464aa660b27a64da491de9d043c1
azar103 <azzarroukanis@gmail.com>
2019-06-26 15:21:03 +0100

    add progess bar

---
+++ app/src/main/java/com/zarrouk/anis/mynews/Adapters/WorldAdapter.java
@@ -0,0 +1,7 @@
+package com.zarrouk.anis.mynews.Adapters;
+
+/**
+ * Created by Anis Zarrouk on 26/06/2019
+ */
+public class WorldAdapter {
+}
---
+++ app/src/main/java/com/zarrouk/anis/mynews/Utils/MyAsyncTask.java
@@ -0,0 +1,7 @@
+package com.zarrouk.anis.mynews.Utils;
+
+/**
+ * Created by Anis Zarrouk on 26/06/2019
+ */
+public class MyAsyncTask {
+}
